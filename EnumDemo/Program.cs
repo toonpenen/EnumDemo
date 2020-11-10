9df1d5183ffd205db2f1700445cf8b49a4c584c7
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-10 09:55:42 +0100

    Enum Edit

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace EnumDemo
 {
+    enum WorkingDays
+    {
+        Monday, //0
+        Tuesday, //1
+        Wednesday, //2
+        Thursday, //3
+        Friday //4
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string day
+
+            //Workingdays d = (WorkingDays)4;
+            //Console.WriteLine(d);
+
+            //WorkingDays d = WorkingDays.Thursday;
+            //Console.WriteLine((int)d);
+
+            // enumerate values 0,1,2..
+            foreach (int i in Enum.GetValues(typeof(WorkingDays)))
+            {
+                Console.WriteLine(i);
+            }
+            // enumerate Names Monday, Tuesday..
+            foreach (string s in Enum.GetNames(typeof(WorkingDays)))
+            {
+                Console.WriteLine(s);
+            }
+
         }
     }
 }
